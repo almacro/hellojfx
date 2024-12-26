5bebb0dbb9664188f0b5c195e1bc71e64d2b14f5
Al Thompson <almacro@acm.org>
2024-12-26 02:59:10 -0800

    Commit project sources

---
+++ README.md
@@ -0,0 +1,5 @@
+# hellojfx
+
+This repository has example projects based on the HelloJFX code samples at [OpenJFX](https://openjfx.io/openjfx-docs).
+
+These samples illustrate structure of JavaFX projects that build using Maven.
---
+++ mvngen-ide.sh
@@ -0,0 +1,8 @@
+mvn archetype:generate \
+    -DarchetypeGroupId=org.openjfx \
+    -DarchetypeArtifactId=javafx-archetype-simple \
+    -DarchetypeVersion=0.0.3 \
+    -DgroupId=org.openjfx \
+    -DartifactId=ide \
+    -Dversion=1.0.0 \
+    -Djavafx-version=23.0.1
